778b2b329d9a7f0544c8fe202cbd6c4a3240cc40
manhir <53478716+manhir@users.noreply.github.com>
2022-12-12 18:33:53 +0300

    add custom mdx components

---
+++ client/lib.tsx
@@ -0,0 +1,38 @@
+import { Button, Title, Text } from '@mantine/core'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Buttons } from '../components/Buttons'
+
+
+export type StaticProps = {
+    source: MDXRemoteSerializeResult
+    lngDict: Record<string, string>
+}
+
+export const components = {
+    img: (props) => (
+        // eslint-disable-next-line jsx-a11y/alt-text
+        <Image
+            {...props}
+        />
+    ),
+    Button: (props) => (
+        <Link
+            href={props.href} passHref
+        >
+            <Button
+                {...props}
+            />
+        </Link>
+    ),
+    Buttons: (props) => <Buttons {...props} />,
+    h1: (props) => (
+        <Title
+            {...props}
+            order={1}
+            style={{ display: 'inline' }}
+        />
+    ),
+    Text: (props) => <Text {...props} />
+}
