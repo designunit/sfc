94fb83e6be25942abb4cd65d99d010999ab32486
Roman Timashev <roman@tmshv.ru>
2023-02-16 17:55:15 +0500

    add de/ar to one more select

--- components/PageLayout/index.tsx
+++ components/PageLayout/index.tsx
@@ -191,6 +191,8 @@ export const PageLayout: React.FC = ({ children }) => {
                                     data={[
                                         { value: "ru", label: "RU" },
                                         { value: "en", label: "EN" },
+                                        { value: "de", label: "DE" },
+                                        { value: "ar", label: "AR" },
                                     ]}
                                 />
                             </MediaQuery>
